8b506e972623f9f2b224f36fea3284f636d6ae5d
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2025-08-30 19:14:47 +0800

    Support MultiDevice

---
+++ Scripts/CreateConfig.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# 添加内存回收补丁
+function addSkbRecycler() {
+  cat >> $1 <<EOF
+# 内存回收补丁
+CONFIG_KERNEL_SKB_RECYCLER=y
+CONFIG_KERNEL_SKB_RECYCLER_MULTI_CPU=y
+EOF
+}
+
+# 添加 eBPF
+function addEBPF() {
+  cat >> $1 <<EOF
+# eBPF
+CONFIG_DEVEL=y
+CONFIG_KERNEL_CGROUPS=y
+CONFIG_KERNEL_CGROUP_BPF=y
+CONFIG_KERNEL_BPF_EVENTS=y
+CONFIG_KERNEL_DEBUG_KERNEL=y
+CONFIG_KERNEL_DEBUG_INFO=y
+CONFIG_KERNEL_DEBUG_INFO_REDUCED=n
+CONFIG_KERNEL_DEBUG_INFO_BTF=y
+CONFIG_KERNEL_XDP_SOCKETS=y
+CONFIG_BPF_TOOLCHAIN_HOST=y
+CONFIG_PACKAGE_kmod-xdp-sockets-diag=y
+EOF
+}
+
+# 高通平台添加 NSS 驱动
+function addNssDriver() {
+  cat >> $1 <<EOF
+# NSS 驱动
+CONFIG_PACKAGE_kmod-qca-nss-macsec=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-vlan=y
+CONFIG_PACKAGE_kmod-qca-nss-dp=y
+CONFIG_PACKAGE_kmod-qca-nss-drv=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-bridge-mgr=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-igs=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-map-t=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-pppoe=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-pptp=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-qdisc=y
+CONFIG_PACKAGE_kmod-qca-nss-ecm=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-l2tpv2=y
+CONFIG_PACKAGE_kmod-qca-nss-drv-lag-mgr=y
+EOF
+}
+
+# 获取内核版本
+function getKernelVersion() {
+  echo $(sed -n 's/^KERNEL_PATCHVER:=\(.*\)/\1/p' target/linux/qualcommax/Makefile)
+}
+
+# 创建配置
+function createConfig() {
+  configFile=".config"
+  # 如配置文件已存在
+  cat $GITHUB_WORKSPACE/Config/${WRT_CONFIG} > $configFile
+  cat $GITHUB_WORKSPACE/Config/CompileFirmware >> $configFile
+  cat $GITHUB_WORKSPACE/Config/ZeroWrtKernelModules >> $configFile
+  if [[ "$WRT_MODE" == 'PACKAGE' ]]; then
+    cat $GITHUB_WORKSPACE/Config/CompilePackage >> $configFile
+  fi
+  # 添加内存回收补丁
+  addSkbRecycler $configFile
+  # 添加 eBPF
+  addEBPF $configFile
+  # 高通平台添加 NSS 驱动
+  if [[ $WRT_TARGET == *"QUALCOMMAX"* ]]; then
+    addNssDriver $configFile
+  fi
+}
--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source $(dirname "$(realpath "$0")")/Function.sh
-
 # 修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 # 修改 immortalwrt.lan 关联 IP
@@ -33,12 +31,11 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 # 修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_HOST'/g" $CFG_FILE
 
-# 修复 Dropbear
-sed -i "/Interface/d" ./package/network/services/dropbear/files/dropbear.config
-
 # 配置文件修改
+echo "CONFIG_PACKAGE_luci=y" >> ./.config
+echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 # 手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
@@ -51,12 +48,12 @@ if [[ $WRT_TARGET == *"QUALCOMMAX"* ]]; then
 	# 取消 NSS 相关 Feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
-	# 设置 NSS 版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
-	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	# 开启 sqm-nss 插件
 	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
 	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
+	# 设置 NSS 版本
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	# 无 WIFI 配置调整 Q6 大小
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
 		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
